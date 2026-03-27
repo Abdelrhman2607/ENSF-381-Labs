adcb1f5277b75bedb0e18f90a8223385121695e4
Maxime Boucher <maxime@maximeboucher.com>
2026-03-27 12:10:20 -0600

    Add lab 8 exercise 2

--- lab8/backend/app.py
+++ lab8/backend/app.py
@@ -55,8 +55,39 @@ users = deepcopy(SEEDED_USERS)
 #   Return 200 for a successful delete and 404 if the user does not
 #   exist. Delete with del users[user_id] after confirming the key
 #   exists.
+
+
 #   Exercise2
 # - POST /predict_house_price
+@app.route('/predict_house_price', methods=['POST'])
+def predict_house_price():
+    try:
+        model = joblib.load(MODEL_PATH)
+        data = request.json
+
+        cats = bool(data['pets'])
+        dogs = bool(data['pets'])
+
+        sample_data = [
+            str(data['city']),
+            str(data['province']),
+            float(data['latitude']),
+            float(data['longitude']),
+            str(data['lease_term']),
+            str(data['type']),
+            float(data['beds']),
+            float(data['baths']),
+            float(data['sq_feet']),
+            str(data['furnishing']),
+            str(data['smoking']),
+            cats,
+            dogs,
+        ]
+        sample_df = pd.DataFrame([sample_data], columns=PREDICTION_COLUMNS)
+        predicted_price = model.predict(sample_df)
+        return jsonify({'predicted_price': float(predicted_price[0])}), 200 
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
